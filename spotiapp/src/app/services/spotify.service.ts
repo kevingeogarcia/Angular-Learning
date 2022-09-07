d89c16c141eb5f80d271229b0389fa7ae1ac0f13
kevingeogarcia <kevingeogarcia@gmail.com>
2022-09-07 17:19:43 -0600

    add services to get artist by name

--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -6,17 +6,24 @@ import { Injectable } from '@angular/core';
 })
 export class SpotifyService {
 
+
+  token:String = "Bearer BQBt6a9bK73Rw7A4xgFrMWXtYOep1dOLHplVC3x2W7SMo6UTKX7eUR5r2v-lheWAiF6hdmx9PYq25JHVvIdvi9QC1xJBCWip_X1YPmdi2xpjt6HPhZc";
   constructor(private http: HttpClient) {
-
-    console.log('esta vaina del service esta listo!');
-
   }
 
   getNewReleases() {
-
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDiShhEOzIb2l83VRy3PwvVhCBJo1Q8VypsPc4jFDHlBgQeTgVQB31mGerrz3f4Dq8CAC6UPO50Fv072wpumMUlrHihs3AhyGGXrVLKcOyXNzV3oRc'
+      'Authorization': `${this.token}`
     });
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
+
+  
+  getArtist(q: String) {
+    const headers = new HttpHeaders({
+      'Authorization': `${this.token}`
+    });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${q}&type=artist`, { headers });
+  }
+  
 }
